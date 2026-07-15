cbf43a73ad7f65e6bdd44a0735d6c5849883262c
A-may-a <amayajayan2005@gmail.com>
2026-07-15 21:45:42 +0530

    Revert " load db config from env"
    This reverts commit 8a01cbb4eb2cb4c99702ee09fbfa3c0ee3bcb873.

--- backend/database.py
+++ backend/database.py
@@ -1,17 +1,13 @@
-import os
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-load_dotenv()
+SQLALCHEMY_DATABASE_URL = "sqlite:///./tasks.db"
 
-# Falls back to local SQLite if DATABASE_URL isn't set in .env
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-
-connect_args = {"check_same_thread": False} if SQLALCHEMY_DATABASE_URL.startswith("sqlite") else {}
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False}  # needed only for SQLite
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
