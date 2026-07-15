dc73d0f1e3a22cce866a29e8b581581804fe24c7
A-may-a <amayajayan2005@gmail.com>
2026-07-15 21:52:40 +0530

    load db config from env

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,17 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./tasks.db"
+load_dotenv()
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}  # needed only for SQLite
-)
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+connect_args = {"check_same_thread": False} if SQLALCHEMY_DATABASE_URL.startswith("sqlite") else {}
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
